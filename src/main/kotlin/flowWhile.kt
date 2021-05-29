12c9e5f5c7b40f3b3f876ead087d0b2d45f9a76c
Matheus Farias <matheusfarias009@hotmail.com>
2021-05-28 21:11:06 -0300

    operação while

---
+++ src/main/kotlin/flowWhile.kt
@@ -0,0 +1,13 @@
+fun main(){
+
+    var index: Int = 0
+    while(index < 100){
+        println("$index - Matheus")
+        index++
+
+        if(index == 50){
+            break
+        }
+    }
+
+}
