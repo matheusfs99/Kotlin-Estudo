360e7623ebc6824203c464486a496f4bd1a85b94
Matheus Farias <matheusfarias009@hotmail.com>
2021-07-28 13:49:56 -0300

    conversão de valores

---
+++ src/main/kotlin/conversaoValores.kt
@@ -0,0 +1,21 @@
+fun teste(n1: Int){
+
+}
+
+
+fun main(){
+
+    val n1: Double = Double.MAX_VALUE
+    val b1: Byte = 100
+
+    println(b1.toShort())
+    println(b1.toDouble())
+    println(b1.toFloat())
+    println(b1.toLong())
+
+    println("19".toInt())
+    println("19".toFloat())
+    println("19".toDouble())
+    println("fdg".toInt())
+
+}
